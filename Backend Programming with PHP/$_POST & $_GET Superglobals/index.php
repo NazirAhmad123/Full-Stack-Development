be5d0db86a75c06cd6632c1a34bb90e4e55244bb
Nazir <nazirahmadsaidzada@gmail.com>
2018-04-29 23:03:57 +0800

    Add files via upload

---
+++ Backend Programming with PHP/$_POST & $_GET Superglobals/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register Form</title>
+	<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+</head>
+<body>
+<div class="w3-card w3-margin w3-padding">
+	<?php if(isset($_GET['error'])): ?>
+		<div class="w3-panel w3-red">
+  			<p><?php echo $_GET['error']; ?></p>
+		</div> 
+	<?php endif; ?>
+	<h2>Register</h2>
+	<form method="POST" action="register.php" class="w3-container">
+	<label class="w3-label w3-text-blue">First Name</label>
+	<input class="w3-input w3-border" name="first_name" type="text">
+	<br>
+	<label class="w3-label w3-text-blue">Last Name</label>
+	<input class="w3-input w3-border" name="last_name" type="text">
+	<br>
+	<label class="w3-label w3-text-blue">Email</label>
+	<input class="w3-input w3-border" name="email" type="text">
+	<br>
+	<label class="w3-label w3-text-blue">Location</label>
+	<select class="w3-input w3-border" name="location">
+		<option value="Location 1">Location 1</option>
+		<option value="Location 2">Location 2</option>
+		<option value="Location 2">Location 2</option>
+	</select>
+	<br>
+	<input class="w3-btn" type="submit" value="Submit">
+	</form>
+</div>
+</body>
+</html>
---
+++ Backend Programming with PHP/$_POST & $_GET Superglobals/register.php
@@ -0,0 +1,27 @@
+<?php
+	$first_name = $_POST['first_name'];
+	$last_name = $_POST['last_name'];
+	$email = $_POST['email'];
+	$location = $_POST['location'];
+
+	if($first_name == '' || $last_name == '' || $email == '' || $location == ''){
+		header("Location: index.php?error=Please%20Fill%20In%20All%20Fields");
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>User Profile</title>
+	<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+</head>
+<body>
+<div class="w3-card w3-margin w3-padding">
+	<h2>User Profile</h2>
+	<h3><?php echo $first_name; ?> <?php echo $last_name; ?></h3>
+	<ul>
+		<li>Email: <?php echo $email; ?></li>
+		<li>Location: <?php echo $location; ?></li>
+	</ul>
+</div>
+</body>
+</html>
